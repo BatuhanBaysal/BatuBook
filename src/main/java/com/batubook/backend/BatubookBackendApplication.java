345e36c6e00924308b0a8ff63afd21ceb6c0e336
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-01 13:27:38 +0300

    Create BatuBook

---
+++ src/main/java/com/batubook/backend/BatubookBackendApplication.java
@@ -0,0 +1,12 @@
+package com.batubook.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatubookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatubookBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/batubook/backend/BatubookBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.batubook.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatubookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
