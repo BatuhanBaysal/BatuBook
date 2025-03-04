99b19c5e2556cfb00f3f298d3ec57265e2868710
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-04 15:20:24 +0300

    Create Genre.java

---
+++ src/main/java/com/batubook/backend/entity/enums/Genre.java
@@ -0,0 +1,13 @@
+package com.batubook.backend.entity.enums;
+
+public enum Genre {
+    NOVEL,
+    ADVENTURE,
+    SCIENCE_FICTION,
+    FANTASY,
+    HORROR,
+    THRILLER,
+    CRIME,
+    DYSTOPIA,
+    ROMANCE;
+}
