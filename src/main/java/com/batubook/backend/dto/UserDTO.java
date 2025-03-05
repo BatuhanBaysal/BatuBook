3febda840341657455a744905d03f16dccf69b70
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-05 14:12:01 +0300

    Create UserDTO.java

---
+++ src/main/java/com/batubook/backend/dto/UserDTO.java
@@ -0,0 +1,25 @@
+package com.batubook.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String role;
+    private UserProfileDTO userProfile;
+    private Set<ReviewDTO> reviews;
+    private Set<QuoteDTO> quotes;
+    private Set<MessageDTO> sentMessages;
+    private Set<MessageDTO> receivedMessages;
+}
