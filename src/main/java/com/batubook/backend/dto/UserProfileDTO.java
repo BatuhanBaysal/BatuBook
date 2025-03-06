02470c9db77b444b0cb546fb47cc49583a50ce54
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-06 13:47:34 +0300

    Create UserProfileDTO.java

---
+++ src/main/java/com/batubook/backend/dto/UserProfileDTO.java
@@ -0,0 +1,22 @@
+package com.batubook.backend.dto;
+
+import com.batubook.backend.entity.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileDTO {
+
+    private Long id;
+    private String dateOfBirth;
+    private Gender gender;
+    private String biography;
+    private String location;
+    private String occupation;
+    private String education;
+}
