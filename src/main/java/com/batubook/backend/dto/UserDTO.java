7ab125f8679e3a62d30413f529f5fa2b4d9a6630
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-09 11:11:40 +0300

    Update UserDTO.java

--- src/main/java/com/batubook/backend/dto/UserDTO.java
+++ src/main/java/com/batubook/backend/dto/UserDTO.java
@@ -16,6 +16,7 @@ public class UserDTO {
     private Long id;
     private String username;
     private String email;
+    private String password;
     private String role;
     private UserProfileDTO userProfile;
     private Set<ReviewDTO> reviews;
