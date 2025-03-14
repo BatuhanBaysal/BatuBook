47fda7498eac63f08f2bd778739d204587093a50
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-14 14:06:58 +0300

    Create ErrorCode.java

---
+++ src/main/java/com/batubook/backend/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.batubook.backend.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    BAD_REQUEST("BAD_REQUEST", "The request is invalid."),
+    UNAUTHORIZED("UNAUTHORIZED", "You are not authorized to perform this action."),
+    FORBIDDEN("FORBIDDEN", "Access to this resource is forbidden."),
+    NOT_FOUND("NOT_FOUND", "The requested resource was not found."),
+    INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", "An internal server error occurred.");
+
+    private final String code;
+    private final String description;
+
+    ErrorCode(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+}
