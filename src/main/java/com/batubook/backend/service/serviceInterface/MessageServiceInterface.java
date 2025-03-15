e03f777bad1960387aa6e06d4ce9a27d06a71a83
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-15 14:31:43 +0300

    Create MessageServiceInterface.java

---
+++ src/main/java/com/batubook/backend/service/serviceInterface/MessageServiceInterface.java
@@ -0,0 +1,16 @@
+package com.batubook.backend.service.serviceInterface;
+
+import com.batubook.backend.dto.MessageDTO;
+import com.batubook.backend.entity.enums.MessageType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface MessageServiceInterface {
+
+    MessageDTO createMessage(MessageDTO messageDTO);
+    MessageDTO getMessageById(Long id);
+    Page<MessageDTO> getAllMessages(Pageable pageable);
+    Page<MessageDTO> getMessageByMessageType(MessageType messageType, Pageable pageable);
+    MessageDTO updateMessage(Long id, MessageDTO messageDTO);
+    void deleteMessage(Long id);
+}
