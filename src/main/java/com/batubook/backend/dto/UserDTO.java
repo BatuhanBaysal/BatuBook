3d00522daa0a99b922fa4d07094e4203460f7a2e
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-17 18:58:15 +0300

    Update UserDTO.java

--- src/main/java/com/batubook/backend/dto/UserDTO.java
+++ src/main/java/com/batubook/backend/dto/UserDTO.java
@@ -5,8 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Set;
-
 @Data
 @Builder
 @NoArgsConstructor
@@ -19,8 +17,4 @@ public class UserDTO {
     private String password;
     private String role;
     private UserProfileDTO userProfile;
-    private Set<ReviewDTO> reviews;
-    private Set<QuoteDTO> quotes;
-    private Set<MessageDTO> sentMessages;
-    private Set<MessageDTO> receivedMessages;
 }
