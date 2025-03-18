3dde34b072022a684ca72821c84df6cec5967d27
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-18 20:39:28 +0300

    Update Gender.java

--- src/main/java/com/batubook/backend/entity/enums/Gender.java
+++ src/main/java/com/batubook/backend/entity/enums/Gender.java
@@ -1,7 +1,33 @@
 package com.batubook.backend.entity.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public enum Gender {
-    MALE,
-    FEMALE,
-    UNDISCLOSED;
+
+    MALE, FEMALE, UNDISCLOSED;
+
+    private static final Logger logger = LoggerFactory.getLogger(Gender.class);
+
+    @JsonCreator
+    public static Gender fromString(String value) {
+        if (value == null) {
+            return UNDISCLOSED;
+        }
+
+        try {
+            return Gender.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            logger.error("Invalid gender value: {}", value);
+            return UNDISCLOSED;
+        }
+    }
+
+    @JsonValue
+    public String toJson() {
+        String enumName = name().toLowerCase();
+        return enumName.replace("ı", "i");
+    }
 }
