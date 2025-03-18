9425a0ca514f49d64a740d1f82258aace4bb534c
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-18 20:34:44 +0300

    Update UserRepository.java

--- src/main/java/com/batubook/backend/repository/UserRepository.java
+++ src/main/java/com/batubook/backend/repository/UserRepository.java
@@ -2,14 +2,14 @@ package com.batubook.backend.repository;
 
 import com.batubook.backend.entity.UserEntity;
 import com.batubook.backend.entity.enums.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    List<UserEntity> findByUsernameAndEmailIgnoreCase(String username, String email);
-    List<UserEntity> findByRole(Role role);
+    Page<UserEntity> findByUsernameAndEmailIgnoreCase(String username, String email, Pageable pageable);
+    Page<UserEntity> findByRole(Role role, Pageable pageable);
 }
