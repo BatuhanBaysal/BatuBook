c11225c84129005e2c2b256a296162afc70d3e4f
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-18 20:35:25 +0300

    Update UserServiceInterface.java

--- src/main/java/com/batubook/backend/service/serviceInterface/UserServiceInterface.java
+++ src/main/java/com/batubook/backend/service/serviceInterface/UserServiceInterface.java
@@ -2,17 +2,19 @@ package com.batubook.backend.service.serviceInterface;
 
 import com.batubook.backend.dto.UserDTO;
 import com.batubook.backend.entity.enums.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface UserServiceInterface {
 
-    UserDTO createUser(UserDTO userDTO);
+    UserDTO registerUser(UserDTO userDTO);
     UserDTO getUserById(Long id);
-    List<UserDTO> getAllUsers();
-    List<UserDTO> getUsersByRole(Role role);
-    List<UserDTO> getUsersByUsernameAndEmail(String username, String email);
+    Page<UserDTO> getAllUsers(Pageable pageable);
+    Page<UserDTO> getUsersByRole(Role role, Pageable pageable);
+    Page<UserDTO> getUsersByUsernameAndEmail(String username, String email, Pageable pageable);
     List<UserDTO> searchUser(String searchTerm);
-    UserDTO updateUser(Long id, UserDTO userDTO);
-    void deleteUserById(Long id);
+    UserDTO modifyUser(Long id, UserDTO userDTO);
+    void removeUserById(Long id);
 }
