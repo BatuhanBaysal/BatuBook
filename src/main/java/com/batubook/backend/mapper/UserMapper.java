172522a19ef189b35c1885eb00005a4b5dcf44ee
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-18 20:33:57 +0300

    Update UserMapper.java

--- src/main/java/com/batubook/backend/mapper/UserMapper.java
+++ src/main/java/com/batubook/backend/mapper/UserMapper.java
@@ -2,7 +2,6 @@ package com.batubook.backend.mapper;
 
 import com.batubook.backend.dto.UserDTO;
 import com.batubook.backend.entity.UserEntity;
-import com.batubook.backend.entity.enums.Role;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -11,22 +10,10 @@ import org.mapstruct.Mapping;
 public interface UserMapper {
 
     @Mapping(target = "password", ignore = true)
-    @Mapping(target = "role", expression = "java(userEntity.getRole().name())")
+    @Mapping(target = "role", source = "role")
     UserDTO userEntityToUserDTO(UserEntity userEntity);
 
     @Mapping(target = "password", ignore = true)
-    @Mapping(target = "role", expression = "java(mapStringToRole(userDTO.getRole()))")
+    @Mapping(target = "role", source = "role")
     UserEntity userDTOToUserEntity(UserDTO userDTO);
-
-    default Role mapStringToRole(String roleString) {
-        try {
-            return roleString != null ? Role.valueOf(roleString.toUpperCase()) : Role.USER;
-        } catch (IllegalArgumentException e) {
-            return Role.USER;
-        }
-    }
-
-    default String mapRoleToString(Role role) {
-        return role != null ? role.name().toUpperCase() : "USER";
-    }
 }
