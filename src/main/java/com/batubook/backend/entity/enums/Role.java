a5d99238a69c0dd4f6adff1bbf0bc0fe9238425d
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-18 20:39:59 +0300

    Update Role.java

--- src/main/java/com/batubook/backend/entity/enums/Role.java
+++ src/main/java/com/batubook/backend/entity/enums/Role.java
@@ -1,6 +1,33 @@
 package com.batubook.backend.entity.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public enum Role {
-    ADMIN,
-    USER;
+
+    ADMIN, USER;
+
+    private static final Logger logger = LoggerFactory.getLogger(Role.class);
+
+    @JsonCreator
+    public static Role fromString(String value) {
+        if (value == null) {
+            return USER;
+        }
+
+        try {
+            return Role.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            logger.error("Invalid role value: {}", value);
+            return USER;
+        }
+    }
+
+    @JsonValue
+    public String toJson() {
+        String enumName = name().toLowerCase();
+        return enumName.replace("ı", "i");
+    }
 }
