6346b73ce3538835c084c5f73092f1d89d451af5
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-19 17:33:01 +0300

    Create FollowEntity.java

---
+++ src/main/java/com/batubook/backend/entity/FollowEntity.java
@@ -0,0 +1,32 @@
+package com.batubook.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "follows", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"follower_id", "followed_user_id"})
+})
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FollowEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id", nullable = false)
+    private UserEntity follower;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "followed_user_id")
+    private UserEntity followedUser;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "followed_book_id")
+    private BookEntity followedBook;
+}
