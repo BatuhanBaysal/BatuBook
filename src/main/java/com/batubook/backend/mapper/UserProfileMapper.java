36d728495910375aab69e87b2d412c9d2811e365
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-03-19 17:29:56 +0300

    Update UserProfileMapper.java

--- src/main/java/com/batubook/backend/mapper/UserProfileMapper.java
+++ src/main/java/com/batubook/backend/mapper/UserProfileMapper.java
@@ -2,7 +2,6 @@ package com.batubook.backend.mapper;
 
 import com.batubook.backend.dto.UserProfileDTO;
 import com.batubook.backend.entity.UserProfileEntity;
-import com.batubook.backend.entity.enums.Gender;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -16,12 +15,4 @@ public interface UserProfileMapper {
     @Mapping(target = "gender", source = "gender")
     @Mapping(target = "dateOfBirth", source = "dateOfBirth", dateFormat = "yyyy-MM-dd")
     UserProfileEntity userProfileDTOToUserProfileEntity(UserProfileDTO userProfileDTO);
-
-    default Gender mapStringToGender(String genderString) {
-        return genderString != null ? Gender.valueOf(genderString.toUpperCase()) : null;
-    }
-
-    default String mapGenderToString(Gender gender) {
-        return gender != null ? gender.name() : null;
-    }
 }
