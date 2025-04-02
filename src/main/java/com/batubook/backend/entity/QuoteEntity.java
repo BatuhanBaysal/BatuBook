a8826257c7b3e868354c50c30f381d10ea42c0af
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-04-02 19:26:53 +0300

    Update QuoteEntity.java

--- src/main/java/com/batubook/backend/entity/QuoteEntity.java
+++ src/main/java/com/batubook/backend/entity/QuoteEntity.java
@@ -1,14 +1,17 @@
 package com.batubook.backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
+
+import java.util.Set;
 
 @Entity
 @Table(name = "quotes")
 @Data
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = true, exclude = {"likes", "repostSaves"})
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,17 +22,25 @@ public class QuoteEntity extends BaseEntity {
     private Long id;
 
     @Column(nullable = false, columnDefinition = "TEXT")
-    @NotNull(message = "Quote text cannot be null.")
+    @NotBlank(message = "Quote text cannot be null or empty.")
     @Pattern(regexp = "^\\S.*\\S$", message = "Quote text cannot have leading or trailing spaces.")
     private String quoteText;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private UserEntity user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "book_id", nullable = false)
     private BookEntity book;
+
+    @OneToMany(mappedBy = "quote", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Set<LikeEntity> likes;
+
+    @OneToMany(mappedBy = "quote", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Set<RepostSaveEntity> repostSaves;
 
     @PrePersist
     @PreUpdate
