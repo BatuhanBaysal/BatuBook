9b0e4e0b13f55f13b09a65ad834127001d2d65d6
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-04-03 18:44:58 +0300

    Create RepostSaveServiceInterface.java

---
+++ src/main/java/com/batubook/backend/service/serviceInterface/RepostSaveServiceInterface.java
@@ -0,0 +1,21 @@
+package com.batubook.backend.service.serviceInterface;
+
+import com.batubook.backend.dto.RepostSaveDTO;
+import com.batubook.backend.entity.enums.ActionType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface RepostSaveServiceInterface {
+
+    RepostSaveDTO registerRepostSave(RepostSaveDTO repostSaveDTO);
+    RepostSaveDTO getRepostSaveById(Long id);
+    Page<RepostSaveDTO> getAllRepostSaves(Pageable pageable);
+    Page<RepostSaveDTO> getByUserId(Long userId, Pageable pageable);
+    Page<RepostSaveDTO> getByUserIdAndActionType(Long userId, ActionType actionType, Pageable pageable);
+    Optional<RepostSaveDTO> getByUserIdAndContent(Long userId, Long reviewId, Long quoteId, Long bookInteractionId);
+    boolean existsByUserIdAndContentAndActionType(Long userId, Long reviewId, Long quoteId, Long bookInteractionId, ActionType actionType);
+    RepostSaveDTO modifyRepostSave(Long id, RepostSaveDTO repostSaveDTO);
+    void removeRepostSave(Long id);
+}
