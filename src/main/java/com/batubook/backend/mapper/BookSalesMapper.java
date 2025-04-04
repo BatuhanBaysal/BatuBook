c5aff82a3ab6e7f15cba643675f0562d1d5ee12f
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-04-04 18:49:11 +0300

    Update BookSalesMapper.java

--- src/main/java/com/batubook/backend/mapper/BookSalesMapper.java
+++ src/main/java/com/batubook/backend/mapper/BookSalesMapper.java
@@ -1,11 +1,9 @@
 package com.batubook.backend.mapper;
 
 import com.batubook.backend.dto.BookSalesDTO;
-import com.batubook.backend.entity.BookEntity;
 import com.batubook.backend.entity.BookSalesEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface BookSalesMapper {
@@ -13,16 +11,6 @@ public interface BookSalesMapper {
     @Mapping(source = "book.id", target = "bookId")
     BookSalesDTO bookSalesEntityToDTO(BookSalesEntity bookSalesEntity);
 
-    @Mapping(source = "bookId", target = "book", qualifiedByName = "mapBookIdToBook")
+    @Mapping(source = "bookId", target = "book.id")
     BookSalesEntity bookSalesDTOToEntity(BookSalesDTO bookSalesDTO);
-
-    @Named("mapBookIdToBook")
-    default BookEntity mapBookIdToBook(Long bookId) {
-        if (bookId == null) {
-            return null;
-        }
-        BookEntity bookEntity = new BookEntity();
-        bookEntity.setId(bookId);
-        return bookEntity;
-    }
 }
