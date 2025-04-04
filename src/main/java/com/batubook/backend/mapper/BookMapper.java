488548f79bf92807062fee3a24b9a53146e1cba5
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-04-04 18:48:31 +0300

    Update BookMapper.java

--- src/main/java/com/batubook/backend/mapper/BookMapper.java
+++ src/main/java/com/batubook/backend/mapper/BookMapper.java
@@ -2,27 +2,16 @@ package com.batubook.backend.mapper;
 
 import com.batubook.backend.dto.BookDTO;
 import com.batubook.backend.entity.BookEntity;
-import com.batubook.backend.entity.enums.Genre;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring",
-        uses = { QuoteMapper.class, ReviewMapper.class })
+        uses = { BookSalesMapper.class, BookInteractionMapper.class, QuoteMapper.class, ReviewMapper.class, FollowMapper.class })
 public interface BookMapper {
 
-    @Mapping(target = "genre", source = "genre")
-    @Mapping(target = "publishDate", expression = "java(String.valueOf(bookEntity.getPublishDate().getYear()))")
-    BookDTO bookEntityToBookDTO(BookEntity bookEntity);
+    @Mapping(target = "publishDate", expression = "java(bookEntity.getPublishDate().toString())")
+    BookDTO bookEntityToDTO(BookEntity bookEntity);
 
-    @Mapping(target = "genre", source = "genre")
-    @Mapping(target = "publishDate", expression = "java(java.time.LocalDate.of(Integer.parseInt(bookDTO.getPublishDate()), 1, 1))")
-    BookEntity bookDTOToBookEntity(BookDTO bookDTO);
-
-    default Genre mapStringToGenre(String genreString) {
-        return genreString != null ? Genre.valueOf(genreString.toUpperCase()) : null;
-    }
-
-    default String mapGenreToString(Genre genre) {
-        return genre != null ? genre.name() : null;
-    }
+    @Mapping(target = "publishDate", expression = "java(java.time.LocalDate.parse(bookDTO.getPublishDate()))")
+    BookEntity bookDTOToEntity(BookDTO bookDTO);
 }
