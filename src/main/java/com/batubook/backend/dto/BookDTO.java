3774a1e36c144eb15984fc08043437e003944d38
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-04-04 18:46:44 +0300

    Update BookDTO.java

--- src/main/java/com/batubook/backend/dto/BookDTO.java
+++ src/main/java/com/batubook/backend/dto/BookDTO.java
@@ -6,8 +6,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Set;
-
 @Data
 @Builder
 @NoArgsConstructor
@@ -15,12 +13,12 @@ import java.util.Set;
 public class BookDTO {
 
     private Long id;
+    private String bookCoverImageUrl;
     private String title;
     private String author;
     private String isbn;
-    private int pageCount;
+    private Integer pageCount;
     private String publishDate;
     private Genre genre;
-    private Set<ReviewDTO> reviews;
-    private Set<QuoteDTO> quotes;
+    private String summary;
 }
