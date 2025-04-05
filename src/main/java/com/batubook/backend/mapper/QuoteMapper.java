1f91035d7dd47609252b3cf3bb89e3340a4c2eb3
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-04-05 17:05:42 +0300

    Update QuoteMapper.java

--- src/main/java/com/batubook/backend/mapper/QuoteMapper.java
+++ src/main/java/com/batubook/backend/mapper/QuoteMapper.java
@@ -3,11 +3,17 @@ package com.batubook.backend.mapper;
 import com.batubook.backend.dto.QuoteDTO;
 import com.batubook.backend.entity.QuoteEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",
+        uses = { RepostSaveMapper.class, LikeMapper.class })
 public interface QuoteMapper {
 
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "book.id", target = "bookId")
     QuoteDTO quoteEntityToQuoteDTO(QuoteEntity quoteEntity);
 
+    @Mapping(source = "userId", target = "user.id")
+    @Mapping(source = "bookId", target = "book.id")
     QuoteEntity quoteDTOToQuoteEntity(QuoteDTO quoteDTO);
 }
