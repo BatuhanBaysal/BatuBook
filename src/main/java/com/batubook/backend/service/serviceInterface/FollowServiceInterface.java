422979575bbf4d9394df2dbc910e490da429ed97
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-04-06 12:58:59 +0300

    Update FollowServiceInterface.java

--- src/main/java/com/batubook/backend/service/serviceInterface/FollowServiceInterface.java
+++ src/main/java/com/batubook/backend/service/serviceInterface/FollowServiceInterface.java
@@ -8,9 +8,10 @@ public interface FollowServiceInterface {
 
     FollowDTO followUser(FollowDTO followDTO);
     FollowDTO followBook(FollowDTO followDTO);
-    boolean unfollowUser(FollowDTO followDTO);
-    boolean unfollowBook(FollowDTO followDTO);
+    Page<FollowDTO> getAllFollows(Pageable pageable);
     Page<FollowDTO> getFollowedUsers(Long followerId, Pageable pageable);
     Page<FollowDTO> getFollowers(Long followedUserId, Pageable pageable);
     Page<FollowDTO> getBookFollowers(Long followedBookId, Pageable pageable);
+    void unfollowUser(FollowDTO followDTO);
+    void unfollowBook(FollowDTO followDTO);
 }
