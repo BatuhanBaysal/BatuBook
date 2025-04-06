61340ba0dcd223453b942ce68902146ae35f0bb2
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-04-06 12:58:21 +0300

    Update BookServiceInterface.java

--- src/main/java/com/batubook/backend/service/serviceInterface/BookServiceInterface.java
+++ src/main/java/com/batubook/backend/service/serviceInterface/BookServiceInterface.java
@@ -6,20 +6,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
 
 public interface BookServiceInterface {
 
-    BookDTO createBook(BookDTO bookDTO);
+    BookDTO registerBook(BookDTO bookDTO);
     BookDTO getBookById(Long id);
     Page<BookDTO> getAllBooks(Pageable pageable);
     Page<BookDTO> getBookByTitleAndAuthor(String title, String author, Pageable pageable);
-    List<BookDTO> searchBook(String searchTerm);
-    Optional<BookDTO> getBookByIsbn(String isbn);
+    Page<BookDTO> getBookByCriteria(String searchTerm, Pageable pageable);
+    BookDTO getBookByIsbn(String isbn);
     Page<BookDTO> getBookByPageCountBetween(int minPageCount, int maxPageCount, Pageable pageable);
     Page<BookDTO> getBookByPublishDateBetween(LocalDate startDate, LocalDate endDate, Pageable pageable);
     Page<BookDTO> getBookByGenre(Genre genre, Pageable pageable);
-    BookDTO updateBook(Long id, BookDTO bookDTO);
-    void deleteBook(Long id);
+    BookDTO modifyBook(Long id, BookDTO bookDTO);
+    void removeBook(Long id);
 }
