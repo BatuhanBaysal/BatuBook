9293ef97468a02fcf2230df70614017cc1e9b1b1
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-04-07 11:29:16 +0300

    Update MessageController.java

--- src/main/java/com/batubook/backend/controller/MessageController.java
+++ src/main/java/com/batubook/backend/controller/MessageController.java
@@ -5,60 +5,73 @@ import com.batubook.backend.entity.enums.MessageType;
 import com.batubook.backend.service.serviceInterface.MessageServiceInterface;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/messages")
 @RequiredArgsConstructor
-@CrossOrigin("*")
-@Slf4j
 public class MessageController {
 
     private final MessageServiceInterface messageService;
+    private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
 
-    @PostMapping("/createMessage")
+    @PostMapping("/create")
     public ResponseEntity<MessageDTO> createMessage(@Valid @RequestBody MessageDTO messageDTO) {
-        log.info("Creating a new message from senderId: {}", messageDTO.getSenderId());
-        MessageDTO createdMessage = messageService.createMessage(messageDTO);
-        return ResponseEntity.ok(createdMessage);
+        logger.info("Received request to create a new message from senderId: {}", messageDTO.getSenderId());
+        MessageDTO createdMessage = messageService.registerMessage(messageDTO);
+        logger.info("Successfully created a new message with ID: {}", createdMessage.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdMessage);
     }
 
-    @GetMapping("/searchMessageId/{id}")
-    public ResponseEntity<MessageDTO> getMessageById(@PathVariable Long id) {
-        log.info("Fetching message with ID: {}", id);
+    @GetMapping("/{id}")
+    public ResponseEntity<MessageDTO> fetchMessageById(@PathVariable Long id) {
+        logger.info("Received GET request for /api/messages/{}", id);
         MessageDTO messageDTO = messageService.getMessageById(id);
+        logger.info("Returned response for message with ID: {}", id);
         return ResponseEntity.ok(messageDTO);
     }
 
-    @GetMapping("/searchAllMessages")
-    public ResponseEntity<Page<MessageDTO>> getAllMessages(Pageable pageable) {
-        log.info("Fetching all messages with pagination");
-        Page<MessageDTO> messages = messageService.getAllMessages(pageable);
+    @GetMapping
+    public ResponseEntity<Page<MessageDTO>> fetchAllMessages(@PageableDefault(size = 5) Pageable pageable) {
+        logger.info("GET api/messages called with pagination: page = {}, size = {}", pageable.getPageNumber(), pageable.getPageSize());
+        Page<MessageDTO> allMessages = messageService.getAllMessages(pageable);
+        logger.info("Successfully fetched {} messages", allMessages.getNumberOfElements());
+        return ResponseEntity.ok(allMessages);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<MessageDTO>> fetchMessagesByType(
+            @RequestParam String messageType,
+            @PageableDefault(size = 5) Pageable pageable) {
+        logger.info("Started fetching messages with type: {}. Page: {}, Size: {}",
+                messageType, pageable.getPageNumber(), pageable.getPageSize());
+        MessageType messageTypeEnum = MessageType.fromString(messageType);
+        Page<MessageDTO> messages = messageService.getMessageByMessageType(messageTypeEnum, pageable);
+        logger.info("Successfully fetched {} messages for type: {}. Total pages: {}, Total elements: {}",
+                messages.getTotalElements(), messageType, messages.getTotalPages(), messages.getTotalElements());
         return ResponseEntity.ok(messages);
     }
 
-    @GetMapping("/searchMessageType/{messageType}")
-    public ResponseEntity<Page<MessageDTO>> getMessagesByType(@PathVariable MessageType messageType, Pageable pageable) {
-        log.info("Fetching messages with type: {}", messageType);
-        Page<MessageDTO> messages = messageService.getMessageByMessageType(messageType, pageable);
-        return ResponseEntity.ok(messages);
-    }
-
-    @PutMapping("/updateMessage/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<MessageDTO> updateMessage(@PathVariable Long id, @Valid @RequestBody MessageDTO messageDTO) {
-        log.info("Updating message with ID: {}", id);
-        MessageDTO updatedMessage = messageService.updateMessage(id, messageDTO);
+        logger.info("Initiating update for message with ID: {}", id);
+        MessageDTO updatedMessage = messageService.modifyMessage(id, messageDTO);
+        logger.info("Successfully updated message with ID: {}. Updated details: {}", id, updatedMessage);
         return ResponseEntity.ok(updatedMessage);
     }
 
-    @DeleteMapping("/deleteMessage/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteMessage(@PathVariable Long id) {
-        log.info("Deleting message with ID: {}", id);
-        messageService.deleteMessage(id);
+        logger.info("Received request to delete message with ID: {}", id);
+        messageService.removeMessage(id);
+        logger.info("Successfully deleted message with ID: {}", id);
         return ResponseEntity.noContent().build();
     }
 }
