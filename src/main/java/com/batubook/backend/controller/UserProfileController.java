2a6010ce44e0fd952e2d104bbd18af57e0647645
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-04-07 11:28:07 +0300

    Update UserProfileController.java

--- src/main/java/com/batubook/backend/controller/UserProfileController.java
+++ src/main/java/com/batubook/backend/controller/UserProfileController.java
@@ -17,64 +17,66 @@ import org.slf4j.Logger;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping("/api/userProfiles")
+@RequestMapping("/api/user-profiles")
 @RequiredArgsConstructor
 public class UserProfileController {
 
     private final UserProfileServiceImpl userProfileService;
     private static final Logger logger = LoggerFactory.getLogger(UserProfileController.class);
 
-    @GetMapping("/userProfileId/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<UserProfileDTO> fetchUserProfileById(@PathVariable Long id) {
-        logger.info("Fetching user profile with ID: {}", id);
+        logger.info("Received GET request for /api/user-profiles/{}", id);
         UserProfileDTO userProfileDTO = userProfileService.getUserProfileById(id);
         logger.info("Successfully retrieved user profile with ID: {}", id);
         return ResponseEntity.ok(userProfileDTO);
     }
 
-    @GetMapping("/allUserProfiles")
-    public ResponseEntity<Page<UserProfileDTO>> fetchAllUserProfiles(@PageableDefault(size = 10) Pageable pageable) {
-        logger.info("Fetching all user profiles");
-        Page<UserProfileDTO> userProfiles = userProfileService.getAllUserProfiles(pageable);
-        logger.info("Successfully fetched {} user profiles", userProfiles.getSize());
-        return ResponseEntity.ok(userProfiles);
+    @GetMapping
+    public ResponseEntity<Page<UserProfileDTO>> fetchAllUserProfiles(@PageableDefault(size = 5) Pageable pageable) {
+        logger.info("GET api/user-profiles called with pagination: page = {}, size = {}", pageable.getPageNumber(), pageable.getPageSize());
+        Page<UserProfileDTO> allUserProfiles = userProfileService.getAllUserProfiles(pageable);
+        logger.info("Successfully fetched {} user profiles", allUserProfiles.getSize());
+        return ResponseEntity.ok(allUserProfiles);
     }
 
     @GetMapping("/search-birthday")
     public ResponseEntity<Page<UserProfileDTO>> fetchUserProfilesByBirthday(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateOfBirth,
-            @PageableDefault(size = 10) Pageable pageable) {
-        logger.info("Fetching user profiles for birthday: {}", dateOfBirth);
-        Page<UserProfileDTO> userProfiles = userProfileService.getUserProfilesByDateOfBirth(dateOfBirth, pageable);
-        logger.info("Successfully fetched {} user profiles for birthday: {}", userProfiles.getSize(), dateOfBirth);
+            @PageableDefault(size = 5) Pageable pageable) {
+        logger.info("Received request to fetch user profiles for birthday: {} with pagination: page {}, size {}",
+                dateOfBirth, pageable.getPageNumber(), pageable.getPageSize());
+        Page<UserProfileDTO> userProfiles = userProfileService.getUserProfilesByBirthDate(dateOfBirth, pageable);
+        logger.info("Successfully fetched {} user profiles for birthday: {} on page {}.",
+                userProfiles.getTotalElements(), dateOfBirth, pageable.getPageNumber());
         return ResponseEntity.ok(userProfiles);
     }
 
     @GetMapping("/search-gender")
     public ResponseEntity<Page<UserProfileDTO>> fetchUserProfilesByGender(
             @RequestParam String gender,
-            @PageableDefault(size = 10) Pageable pageable) {
+            @PageableDefault(size = 5) Pageable pageable) {
         Gender genderEnum = Gender.fromString(gender);
-        logger.info("Fetching user profiles for gender: {}", genderEnum);
+        logger.info("Received request to fetch user profiles for gender: {} with pagination: page {}, size {}",
+                genderEnum, pageable.getPageNumber(), pageable.getPageSize());
         Page<UserProfileDTO> userProfiles = userProfileService.getUserProfilesByGender(genderEnum, pageable);
-        logger.info("Successfully fetched {} user profiles for gender: {}", userProfiles.getSize(), genderEnum);
+        logger.info("Successfully fetched {} user profiles for gender: {} on page {}.",
+                userProfiles.getTotalElements(), genderEnum, pageable.getPageNumber());
         return ResponseEntity.ok(userProfiles);
     }
 
-    @PutMapping("/updateUserProfile/{id}")
-    public ResponseEntity<UserProfileDTO> updateUserProfile(
-            @PathVariable Long id,
-            @Valid @RequestBody UserProfileDTO userProfileDTO) {
+    @PutMapping("/update/{id}")
+    public ResponseEntity<UserProfileDTO> updateUserProfile(@PathVariable Long id, @Valid @RequestBody UserProfileDTO userProfileDTO) {
         logger.info("Updating user profile with ID: {}", id);
         UserProfileDTO updatedUserProfile = userProfileService.modifyUserProfile(id, userProfileDTO);
         logger.info("Successfully updated user profile with ID: {}", id);
         return ResponseEntity.ok(updatedUserProfile);
     }
 
-    @DeleteMapping("/deleteUserProfile/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteUserProfile(@PathVariable Long id) {
-        logger.info("Deleting user profile with ID: {}", id);
-        userProfileService.removeUserProfileById(id);
+        logger.info("Received request to delete user profile with ID: {}", id);
+        userProfileService.removeUserProfile(id);
         logger.info("Successfully deleted user profile with ID: {}", id);
         return ResponseEntity.noContent().build();
     }
