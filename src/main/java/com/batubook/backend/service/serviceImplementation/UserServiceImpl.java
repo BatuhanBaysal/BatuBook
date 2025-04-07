7eeab55ad93e59dd08086f006f1d5d814edb9ea7
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-04-07 11:24:58 +0300

    Update UserServiceImpl.java

--- src/main/java/com/batubook/backend/service/serviceImplementation/UserServiceImpl.java
+++ src/main/java/com/batubook/backend/service/serviceImplementation/UserServiceImpl.java
@@ -5,7 +5,6 @@ import com.batubook.backend.entity.UserEntity;
 import com.batubook.backend.entity.UserProfileEntity;
 import com.batubook.backend.entity.enums.Gender;
 import com.batubook.backend.entity.enums.Role;
-import com.batubook.backend.exception.CustomException;
 import com.batubook.backend.exception.CustomExceptions;
 import com.batubook.backend.mapper.UserMapper;
 import com.batubook.backend.mapper.UserProfileMapper;
@@ -14,6 +13,7 @@ import com.batubook.backend.repository.UserRepository;
 import com.batubook.backend.service.serviceInterface.UserServiceInterface;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
@@ -21,8 +21,8 @@ import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -38,13 +38,12 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserServiceInterface {
 
     private final UserRepository userRepository;
-    private final UserProfileRepository userProfileRepository;
-
     private final UserMapper userMapper;
-    private final UserProfileMapper userProfileMapper;
-
     private final PasswordEncoder passwordEncoder;
     private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    private final UserProfileRepository userProfileRepository;
+    private final UserProfileMapper userProfileMapper;
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -56,8 +55,7 @@ public class UserServiceImpl implements UserServiceInterface {
 
         try {
             validateUserDTO(userDTO);
-
-            UserEntity userEntity = userMapper.userDTOToUserEntity(userDTO);
+            UserEntity userEntity = userMapper.userDTOToEntity(userDTO);
             userEntity.setPassword(passwordEncoder.encode(userDTO.getPassword()));
 
             UserProfileEntity userProfileEntity = mapUserProfile(userDTO);
@@ -66,177 +64,85 @@ public class UserServiceImpl implements UserServiceInterface {
 
             UserEntity savedUser = userRepository.save(userEntity);
             logger.info("User created successfully: {}", userDTO.getUsername());
-            return userMapper.userEntityToUserDTO(savedUser);
+            return userMapper.userEntityToDTO(savedUser);
 
         } catch (CustomExceptions.BadRequestException e) {
-            logger.error("Bad request while creating user: {}", userDTO.getUsername(), e);
+            logger.error("Bad Request Error: {}", e.getMessage());
             throw e;
         } catch (Exception e) {
-            logger.error("Error occurred while creating user: {}", userDTO.getUsername(), e);
-            throw new CustomExceptions.InternalServerErrorException("An unexpected error occurred while creating the user.");
+            logger.error("Error while creating user: {}", e.getMessage());
+            throw new CustomExceptions.InternalServerErrorException("User could not be created: " + e.getMessage());
         }
-    }
-
-    private void validateUserDTO(UserDTO userDTO) {
-        if (userDTO.getPassword() == null || userDTO.getPassword().isBlank()) {
-            logger.error("Password is empty for user: {}", userDTO.getUsername());
-            throw new CustomExceptions.BadRequestException("Password cannot be empty.");
-        }
-
-        if (userDTO.getUserProfile() == null) {
-            logger.error("User profile is missing for user: {}", userDTO.getUsername());
-            throw new CustomExceptions.BadRequestException("User profile is required.");
-        }
-    }
-
-    private UserProfileEntity mapUserProfile(UserDTO userDTO) {
-        Gender genderEnum = Gender.fromString(userDTO.getUserProfile().getGender().toString());
-        userDTO.getUserProfile().setGender(genderEnum);
-        return userProfileMapper.userProfileDTOToUserProfileEntity(userDTO.getUserProfile());
     }
 
     @Override
     @Transactional(readOnly = true)
     public UserDTO getUserById(Long id) {
-        logger.info("Fetching user with ID: {}", id);
+        logger.info("Attempting to retrieve user with ID: {}", id);
+        UserEntity userById = userRepository.findById(id)
+                .orElseThrow(() -> {
+                    logger.warn("User not found with ID: {}", id);
+                    return new CustomExceptions.NotFoundException("User not found with ID: " + id);
+                });
 
-        try {
-            UserEntity userEntity = userRepository.findById(id)
-                    .orElseThrow(() -> new CustomExceptions.NotFoundException("User not found with ID: " + id));
-
-            logger.info("User retrieved successfully with ID: {}", id);
-            return userMapper.userEntityToUserDTO(userEntity);
-
-        } catch (CustomExceptions.NotFoundException e) {
-            logger.error("User not found for ID: {}", id, e);
-            throw e;
-        } catch (Exception e) {
-            logger.error("Error occurred while retrieving user with ID: {}", id, e);
-            throw new CustomExceptions.InternalServerErrorException("An unexpected error occurred while fetching the user.");
-        }
+        logger.info("Successfully retrieved user with ID: {}", id);
+        return userMapper.userEntityToDTO(userById);
     }
 
     @Override
     @Transactional(readOnly = true)
     public Page<UserDTO> getAllUsers(Pageable pageable) {
-        logger.info("Fetching all users with pagination: {}", pageable);
-
-        try {
-            Page<UserEntity> users = userRepository.findAll(pageable);
-            if (users.isEmpty()) {
-                logger.warn("No users found for the given pagination parameters");
-                throw new CustomExceptions.NotFoundException("No users found.");
-            }
-
-            logger.info("Successfully fetched {} users", users.getSize());
-            return users.map(userMapper::userEntityToUserDTO);
-
-        } catch (CustomExceptions.NotFoundException e) {
-            logger.error("No users found", e);
-            throw e;
-        } catch (Exception e) {
-            logger.error("Unexpected error occurred while fetching all users: {}", e.getMessage(), e);
-            throw new CustomExceptions.InternalServerErrorException("An unexpected error occurred while fetching all users.");
-        }
+        logger.debug("Fetching all users with pagination: page = {}, size = {}", pageable.getPageNumber(), pageable.getPageSize());
+        Page<UserEntity> allUsers = userRepository.findAll(pageable);
+        logger.info("Successfully fetched {} users", allUsers.getNumberOfElements());
+        return allUsers.map(userMapper::userEntityToDTO);
     }
 
     @Override
     @Transactional(readOnly = true)
     public Page<UserDTO> getUsersByRole(Role role, Pageable pageable) {
         logger.info("Fetching users with role: {} and pagination: {}", role, pageable);
-
-        if (role == null) {
-            logger.error("Role is null");
-            throw new CustomExceptions.BadRequestException("Role must be provided.");
-        }
-
-        try {
-            Page<UserEntity> users = userRepository.findByRole(role, pageable);
-            if (users.isEmpty()) {
-                logger.warn("No users found for role: {}", role);
-                throw new CustomExceptions.NotFoundException("No users found with the provided role.");
-            }
-
-            logger.info("Successfully fetched {} users with role {}", users.getSize(), role);
-            return users.map(userMapper::userEntityToUserDTO);
-
-        } catch (CustomExceptions.NotFoundException e) {
-            logger.error("No users found for role: {}", role, e);
-            throw e;
-        } catch (Exception e) {
-            logger.error("Unexpected error occurred while fetching users with role {}: {}", role, e.getMessage(), e);
-            throw new CustomExceptions.InternalServerErrorException("An unexpected error occurred while fetching users.");
-        }
+        Page<UserEntity> users = userRepository.findByRole(role, pageable);
+        logger.info("Successfully fetched {} users with role {}", users.getNumberOfElements(), role);
+        return users.map(userMapper::userEntityToDTO);
     }
 
     @Override
     @Transactional(readOnly = true)
     public Page<UserDTO> getUsersByUsernameAndEmail(String username, String email, Pageable pageable) {
         logger.info("Fetching users by username: {} and email: {} with pagination: {}", username, email, pageable);
-
-        if (username == null || username.trim().isEmpty() || email == null || email.trim().isEmpty()) {
-            logger.error("Invalid input: username or email is empty");
-            throw new CustomExceptions.BadRequestException("Username and email must be provided.");
-        }
-
-        try {
-            Page<UserEntity> users = userRepository.findByUsernameAndEmailIgnoreCase(username, email, pageable);
-            if (users.isEmpty()) {
-                logger.warn("No users found with username: {} and email: {}", username, email);
-                throw new CustomExceptions.NotFoundException("No users found with the provided username and email.");
-            }
-
-            logger.info("Successfully fetched {} users for username: {} and email: {}", users.getSize(), username, email);
-            return users.map(userMapper::userEntityToUserDTO);
-
-        } catch (CustomExceptions.NotFoundException e) {
-            logger.error("No users found for username: {} and email: {}", username, email, e);
-            throw e;
-        } catch (Exception e) {
-            logger.error("Unexpected error occurred while fetching users by username: {} and email: {}: {}", username, email, e.getMessage(), e);
-            throw new CustomExceptions.InternalServerErrorException("An unexpected error occurred while fetching users.");
-        }
+        Page<UserEntity> users = userRepository.findByUsernameAndEmailIgnoreCase(username, email, pageable);
+        logger.info("Successfully fetched {} users for username: {} and email: {}", users.getNumberOfElements(), username, email);
+        return users.map(userMapper::userEntityToDTO);
     }
 
     @Override
     @Transactional(readOnly = true)
-    public List<UserDTO> searchUser(String searchTerm) {
-        logger.info("Searching users with search term: {}", searchTerm);
-        if (searchTerm == null || searchTerm.trim().isEmpty()) {
-            logger.error("Search term is empty");
-            throw new CustomExceptions.BadRequestException("Search term must be provided.");
-        }
-
+    public Page<UserDTO> getUserByCriteria(String searchTerm, Pageable pageable) {
+        logger.info("Searching users with search term: '{}' using pagination: page number = {}, page size = {}",
+                searchTerm, pageable.getPageNumber(), pageable.getPageSize());
         try {
             CriteriaBuilder cb = entityManager.getCriteriaBuilder();
             CriteriaQuery<UserEntity> query = cb.createQuery(UserEntity.class);
             Root<UserEntity> user = query.from(UserEntity.class);
 
-            String lowerCaseSearchTerm = "%" + searchTerm.trim().toLowerCase() + "%";
-
-            Predicate usernamePredicate = cb.like(cb.lower(user.get("username")), lowerCaseSearchTerm);
-            Predicate emailPredicate = cb.like(cb.lower(user.get("email")), lowerCaseSearchTerm);
-
+            Predicate usernamePredicate = cb.like(cb.lower(user.get("username")), "%" + searchTerm.toLowerCase() + "%");
+            Predicate emailPredicate = cb.like(cb.lower(user.get("email")), "%" + searchTerm.toLowerCase() + "%");
             query.where(cb.or(usernamePredicate, emailPredicate));
 
-            List<UserEntity> users = entityManager.createQuery(query).getResultList();
+            TypedQuery<UserEntity> typedQuery = entityManager.createQuery(query);
+            typedQuery.setFirstResult((int) pageable.getOffset());
+            typedQuery.setMaxResults(pageable.getPageSize());
 
-            if (users.isEmpty()) {
-                logger.error("No users found for search term: {}", searchTerm);
-                throw new CustomExceptions.NotFoundException("No users found with the provided search term.");
-            }
+            List<UserEntity> users = typedQuery.getResultList();
+            logger.info("Found {} users for search term '{}'", users.size(), searchTerm);
+            return new PageImpl<>(users.stream()
+                    .map(userMapper::userEntityToDTO)
+                    .collect(Collectors.toList()), pageable, users.size());
 
-            logger.info("Successfully found {} users for search term: {}", users.size(), searchTerm);
-            return users.stream()
-                    .map(userMapper::userEntityToUserDTO)
-                    .collect(Collectors.toList());
-
-        } catch (CustomExceptions.NotFoundException e) {
-            logger.error("No users found for search term: {}", searchTerm, e);
-            throw e;
         } catch (Exception e) {
-            logger.error("Unexpected error occurred while searching users with search term: {}. Error message: {}", searchTerm, e.getMessage(), e);
-            throw new CustomExceptions.InternalServerErrorException("An unexpected error occurred while searching users.");
+            logger.error("Error occurred while searching for users with search term '{}'", searchTerm, e);
+            throw new RuntimeException("Error occurred while searching for users", e);
         }
     }
 
@@ -251,7 +157,6 @@ public class UserServiceImpl implements UserServiceInterface {
                     });
 
             modifyUserDetails(existingUser, userDTO);
-
             if (userDTO.getUserProfile() == null) {
                 logger.error("User profile is required for update, user ID: {}", id);
                 throw new CustomExceptions.BadRequestException("User profile is required for update.");
@@ -260,41 +165,66 @@ public class UserServiceImpl implements UserServiceInterface {
             modifyUserProfile(existingUser, userDTO);
             UserEntity updatedUser = userRepository.save(existingUser);
             logger.info("User updated successfully with ID: {}", id);
-            return userMapper.userEntityToUserDTO(updatedUser);
+            return userMapper.userEntityToDTO(updatedUser);
 
-        } catch (CustomException e) {
-            logger.error("Custom exception occurred while updating user with ID: {}", id, e);
+        } catch (CustomExceptions.NotFoundException e) {
+            logger.error("User not found: {}", e.getMessage());
             throw e;
         } catch (Exception e) {
-            logger.error("Unexpected error occurred while updating user with ID: {}", id, e);
-            throw new CustomExceptions.InternalServerErrorException("An unexpected error occurred while updating user.");
+            logger.error("Error while updating user: {}", e.getMessage());
+            throw new CustomExceptions.InternalServerErrorException("User could not be updated: " + e.getMessage());
         }
+    }
+
+    @Override
+    @Transactional
+    public void removeUser(Long id) {
+        logger.info("Attempting to remove user with ID: {}", id);
+        if (!userRepository.existsById(id)) {
+            logger.error("User with ID: {} not found for deletion", id);
+            throw new CustomExceptions.NotFoundException("User not found with ID: " + id);
+        }
+
+        userRepository.deleteById(id);
+        logger.info("Successfully deleted user with ID: {}", id);
+    }
+
+    private void validateUserDTO(UserDTO userDTO) {
+        if (userDTO.getPassword() == null || userDTO.getPassword().isBlank()) {
+            logger.error("Password is empty for user: {}", userDTO.getUsername());
+            throw new CustomExceptions.BadRequestException("Password cannot be empty.");
+        }
+        if (userDTO.getUserProfile() == null) {
+            logger.error("User profile is missing for user: {}", userDTO.getUsername());
+            throw new CustomExceptions.BadRequestException("User profile is required.");
+        }
+    }
+
+    private UserProfileEntity mapUserProfile(UserDTO userDTO) {
+        Gender genderEnum = Gender.fromString(userDTO.getUserProfile().getGender().toString());
+        userDTO.getUserProfile().setGender(genderEnum);
+        return userProfileMapper.userProfileDTOToEntity(userDTO.getUserProfile());
     }
 
     private void modifyUserDetails(UserEntity existingUser, UserDTO userDTO) {
         boolean isUpdated = false;
-
         if (userDTO.getUsername() != null && !userDTO.getUsername().isEmpty() && !userDTO.getUsername().equals(existingUser.getUsername())) {
             existingUser.setUsername(userDTO.getUsername());
             isUpdated = true;
         }
-
         if (userDTO.getEmail() != null && !userDTO.getEmail().isEmpty() && !userDTO.getEmail().equals(existingUser.getEmail())) {
             existingUser.setEmail(userDTO.getEmail());
             isUpdated = true;
         }
-
         if (userDTO.getPassword() != null && !userDTO.getPassword().isEmpty()) {
             existingUser.setPassword(passwordEncoder.encode(userDTO.getPassword()));
             logger.info("Password hashed and updated for user ID: {}", existingUser.getId());
             isUpdated = true;
         }
-
         if (userDTO.getRole() != null && !userDTO.getRole().equals(existingUser.getRole())) {
             existingUser.setRole(userDTO.getRole());
             isUpdated = true;
         }
-
         if (isUpdated) {
             logger.info("User details updated for user ID: {}", existingUser.getId());
         }
@@ -302,9 +232,10 @@ public class UserServiceImpl implements UserServiceInterface {
 
     private void modifyUserProfile(UserEntity existingUser, UserDTO userDTO) {
         if (existingUser.getUserProfile() == null) {
-            UserProfileEntity newUserProfile = userProfileMapper.userProfileDTOToUserProfileEntity(userDTO.getUserProfile());
+            UserProfileEntity newUserProfile = userProfileMapper.userProfileDTOToEntity(userDTO.getUserProfile());
             newUserProfile.setUser(existingUser);
             existingUser.setUserProfile(newUserProfile);
+
             userProfileRepository.save(newUserProfile);
             logger.info("Created new user profile for user ID: {}", existingUser.getId());
         } else {
@@ -359,24 +290,6 @@ public class UserServiceImpl implements UserServiceInterface {
                 userProfileRepository.save(userProfileEntity);
                 logger.info("User profile updated for user ID: {}", existingUser.getId());
             }
-        }
-    }
-
-    @Override
-    @Transactional
-    public void removeUserById(Long id) {
-        logger.info("Deleting user with ID: {}", id);
-
-        try {
-            userRepository.deleteById(id);
-            logger.info("User deleted successfully with ID: {}", id);
-
-        } catch (EmptyResultDataAccessException e) {
-            logger.error("User not found for delete with ID: {}", id, e);
-            throw new CustomExceptions.NotFoundException("User not found with ID: " + id);
-        } catch (Exception e) {
-            logger.error("Error occurred while deleting user with ID: {}", id, e);
-            throw new CustomExceptions.InternalServerErrorException("An unexpected error occurred while deleting the user.");
         }
     }
 }
