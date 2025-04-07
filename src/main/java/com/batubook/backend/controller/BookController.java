f3e7d5cdb5077d707cfbea93c2f4c51e04893c2a
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-04-07 11:28:40 +0300

    Update BookController.java

--- src/main/java/com/batubook/backend/controller/BookController.java
+++ src/main/java/com/batubook/backend/controller/BookController.java
@@ -9,196 +9,124 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
 @RequiredArgsConstructor
-@CrossOrigin("*")
 public class BookController {
 
     private final BookServiceImpl bookService;
     private static final Logger logger = LoggerFactory.getLogger(BookController.class);
 
-    @PostMapping("/createBook")
+    @PostMapping("/create")
     public ResponseEntity<BookDTO> createBook(@Valid @RequestBody BookDTO bookDTO) {
-
         logger.info("Received request to create a new book with title: {}", bookDTO.getTitle());
-        try {
-            BookDTO createdBook = bookService.createBook(bookDTO);
-            logger.info("Successfully created book with ID: {}", createdBook.getId());
-            return ResponseEntity.status(HttpStatus.CREATED).body(createdBook);
-        } catch (Exception e) {
-            logger.error("Error occurred while creating book: {}", e.getMessage(), e);
-            throw e;
-        }
+        BookDTO createdBook = bookService.registerBook(bookDTO);
+        logger.info("Successfully created book with ID: {}", createdBook.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdBook);
     }
 
-    @GetMapping("/bookId/{id}")
-    public ResponseEntity<BookDTO> searchBookById(@PathVariable Long id) {
-
-        logger.info("Received request to search for book with ID: {}", id);
-        try {
-            BookDTO searchBookId = bookService.getBookById(id);
-            logger.info("Successfully retrieved book with ID: {}", id);
-            return ResponseEntity.ok(searchBookId);
-        } catch (Exception e) {
-            logger.error("Error occurred while retrieving book with ID: {}", id, e);
-            throw e;
-        }
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDTO> fetchBookById(@PathVariable Long id) {
+        logger.info("Received GET request for /api/books/{}", id);
+        BookDTO bookDTO = bookService.getBookById(id);
+        logger.info("Returned response for book with ID: {}", id);
+        return ResponseEntity.ok(bookDTO);
     }
 
-    @GetMapping("/allBooks")
-    public ResponseEntity<Page<BookDTO>> searchAllBooks(Pageable pageable) {
-
-        logger.info("Received request to fetch all books with pagination: Page number = {}, Page size = {}", pageable.getPageNumber(), pageable.getPageSize());
-        try {
-            Page<BookDTO> allBooks = bookService.getAllBooks(pageable);
-            logger.info("Successfully fetched {} books", allBooks.getTotalElements());
-            return ResponseEntity.ok(allBooks);
-        } catch (Exception e) {
-            logger.error("Error occurred while fetching books with pagination", e);
-            throw e;
-        }
+    @GetMapping
+    public ResponseEntity<Page<BookDTO>> fetchAllBooks(@PageableDefault(size = 5) Pageable pageable) {
+        logger.info("GET api/books called with pagination: page = {}, size = {}", pageable.getPageNumber(), pageable.getPageSize());
+        Page<BookDTO> allBooks = bookService.getAllBooks(pageable);
+        logger.info("Successfully fetched {} books", allBooks.getNumberOfElements());
+        return ResponseEntity.ok(allBooks);
     }
 
     @GetMapping("/BookTitleAndAuthor")
-    public ResponseEntity<Page<BookDTO>> searchBookByTitleAndAuthor(
-            @RequestParam String title, @RequestParam String author, Pageable pageable) {
-
-        logger.info("Received request to search books by title and author: title = {}, author = {}, Page number = {}, Page size = {}",
+    public ResponseEntity<Page<BookDTO>> fetchBookByTitleAndAuthor(
+            @RequestParam String title,
+            @RequestParam String author,
+            @PageableDefault(size = 5) Pageable pageable) {
+        logger.debug("Received request to search books by title and author: title = '{}', author = '{}', Page number = {}, Page size = {}",
                 title, author, pageable.getPageNumber(), pageable.getPageSize());
-        try {
-            Page<BookDTO> titleAndAuthor = bookService.getBookByTitleAndAuthor(title, author, pageable);
-            logger.info("Successfully fetched {} books for title: '{}' and author: '{}'", titleAndAuthor.getTotalElements(), title, author);
-            return ResponseEntity.ok(titleAndAuthor);
-        } catch (Exception e) {
-            logger.error("Error occurred while searching books by title: '{}' and author: '{}'", title, author, e);
-            throw e;
-        }
+        Page<BookDTO> titleAndAuthor = bookService.getBookByTitleAndAuthor(title, author, pageable);
+        logger.info("Successfully fetched {} books for title: '{}' and author: '{}'", titleAndAuthor.getTotalElements(), title, author);
+        return ResponseEntity.ok(titleAndAuthor);
     }
 
-    @GetMapping("/searchTerm")
-    public ResponseEntity<List<BookDTO>> searchTerm(@RequestParam String searchTerm) {
-
-        logger.info("Received request to search books with search term: '{}'", searchTerm);
-        try {
-            List<BookDTO> searchBook = bookService.searchBook(searchTerm);
-            logger.info("Successfully fetched {} books matching the search term: '{}'", searchBook.size(), searchTerm);
-
-            return ResponseEntity.ok(searchBook);
-        } catch (Exception e) {
-            logger.error("Error occurred while searching books with search term: '{}'", searchTerm, e);
-            throw e;
-        }
+    @GetMapping("/search")
+    public ResponseEntity<Page<BookDTO>> fetchBooksByCriteria(
+            @RequestParam String searchTerm,
+            @PageableDefault(size = 5) Pageable pageable) {
+        logger.info("Received request to search for books with search term: '{}' and pagination: Page number = {}, Page size = {}",
+                searchTerm, pageable.getPageNumber(), pageable.getPageSize());
+        Page<BookDTO> books = bookService.getBookByCriteria(searchTerm, pageable);
+        logger.info("Successfully fetched {} books matching the search term: '{}'", books.getTotalElements(), searchTerm);
+        return ResponseEntity.ok(books);
     }
 
-    @GetMapping("/bookIsbn/{isbn}")
-    public ResponseEntity<BookDTO> searchBookByIsbn(@PathVariable String isbn) {
-
+    @GetMapping("/Isbn/{isbn}")
+    public ResponseEntity<BookDTO> fetchBookByIsbn(@PathVariable String isbn) {
         logger.info("Received request to search for book with ISBN: '{}'", isbn);
-        try {
-            Optional<BookDTO> bookDTO = bookService.getBookByIsbn(isbn);
-            if (bookDTO.isPresent()) {
-                logger.info("Successfully found book with ISBN: '{}'", isbn);
-                return ResponseEntity.ok(bookDTO.get());
-            } else {
-                logger.info("No book found with ISBN: '{}'", isbn);
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-            }
-        } catch (Exception e) {
-            logger.error("Error occurred while searching for book with ISBN: '{}'", isbn, e);
-            throw e;
-        }
+        BookDTO bookDTO = bookService.getBookByIsbn(isbn);
+        logger.info("Successfully found book with ISBN: '{}'", isbn);
+        return ResponseEntity.ok(bookDTO);
     }
 
     @GetMapping("/pageCountBetween")
-    public ResponseEntity<Page<BookDTO>> searchBooksByPageCountBetween(
-            @RequestParam int minPageCount, @RequestParam int maxPageCount, Pageable pageable) {
-
+    public ResponseEntity<Page<BookDTO>> fetchBooksByPageCountBetween(
+            @RequestParam int minPageCount,
+            @RequestParam int maxPageCount,
+            @PageableDefault(size = 5) Pageable pageable) {
         logger.info("Received request to search books with page count between {} and {}", minPageCount, maxPageCount);
-        try {
-            Page<BookDTO> books = bookService.getBookByPageCountBetween(minPageCount, maxPageCount, pageable);
-            return ResponseEntity.ok(books);
-        } catch (Exception e) {
-            logger.error("Error occurred while searching books with page count between {} and {}", minPageCount, maxPageCount, e);
-            throw e;
-        }
+        Page<BookDTO> books = bookService.getBookByPageCountBetween(minPageCount, maxPageCount, pageable);
+        logger.info("Successfully fetched {} books with page count between {} and {}", books.getTotalElements(), minPageCount, maxPageCount);
+        return ResponseEntity.ok(books);
     }
 
     @GetMapping("/publishDateBetween")
-    public ResponseEntity<Page<BookDTO>> searchBooksByPublishDateBetween(
+    public ResponseEntity<Page<BookDTO>> fetchBooksByPublishDateBetween(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
-            Pageable pageable) {
-
+            @PageableDefault(size = 5) Pageable pageable) {
         logger.info("Received request to search books published between '{}' and '{}'", startDate, endDate);
-        try {
-            Page<BookDTO> books = bookService.getBookByPublishDateBetween(startDate, endDate, pageable);
-            return ResponseEntity.ok(books);
-        } catch (Exception e) {
-            logger.error("Error occurred while searching books published between '{}' and '{}'", startDate, endDate, e);
-            throw e;
-        }
+        Page<BookDTO> books = bookService.getBookByPublishDateBetween(startDate, endDate, pageable);
+        logger.info("Successfully fetched {} books published between '{}' and '{}'", books.getTotalElements(), startDate, endDate);
+        return ResponseEntity.ok(books);
     }
 
     @GetMapping("/genre")
-    public ResponseEntity<Page<BookDTO>> searchBooksByGenre(
-            @RequestParam String genre, Pageable pageable) {
-
-        try {
-            Genre genreEnum = null;
-            try {
-                genreEnum = Genre.valueOf(genre.toUpperCase().replace("İ", "I"));
-            } catch (IllegalArgumentException e) {
-                logger.error("Invalid genre value: {}. Please provide a valid genre.", genre, e);
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-            }
-
-            logger.info("Received request to search books of genre '{}'", genreEnum);
-            Page<BookDTO> books = bookService.getBookByGenre(genreEnum, pageable);
-            return ResponseEntity.ok(books);
-
-        } catch (Exception e) {
-            logger.error("Error occurred while searching books of genre '{}'", genre, e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
+    public ResponseEntity<Page<BookDTO>> fetchBooksByGenre(
+            @RequestParam String genre,
+            @PageableDefault(size = 5) Pageable pageable) {
+        Genre genreEnum = Genre.fromString(genre);
+        logger.info("Received request to search books of genre '{}' with pagination: page number = {}, page size = {}",
+                genreEnum, pageable.getPageNumber(), pageable.getPageSize());
+        Page<BookDTO> books = bookService.getBookByGenre(genreEnum, pageable);
+        logger.info("Successfully fetched {} books for genre '{}'", books.getTotalElements(), genreEnum);
+        return ResponseEntity.ok(books);
     }
 
-    @PutMapping("/updateBook/{id}")
-    public ResponseEntity<BookDTO> updateBook(
-            @PathVariable Long id,
-            @Valid @RequestBody BookDTO bookDTO) {
-
+    @PutMapping("/update/{id}")
+    public ResponseEntity<BookDTO> updateBook(@PathVariable Long id, @Valid @RequestBody BookDTO bookDTO) {
         logger.info("Received request to update book with id: {}", id);
-        try {
-            BookDTO updatedBook = bookService.updateBook(id, bookDTO);
-            logger.info("Successfully updated book with id: {}", id);
-            return ResponseEntity.ok(updatedBook);
-        } catch (Exception e) {
-            logger.error("Error occurred while updating book with id: {}", id, e);
-            throw e;
-        }
+        BookDTO updatedBook = bookService.modifyBook(id, bookDTO);
+        logger.info("Successfully updated book with id: {}", id);
+        return ResponseEntity.ok(updatedBook);
     }
 
-    @DeleteMapping("/deleteBook/{id}")
-    public ResponseEntity<String> deleteBook(@PathVariable Long id) {
-
-        logger.info("Received request to delete book with id: {}", id);
-        try {
-            bookService.deleteBook(id);
-            logger.info("Successfully deleted book with id: {}", id);
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Book deleted successfully");
-        } catch (Exception e) {
-            logger.error("Error occurred while deleting book with id: {}", id, e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while deleting the book");
-        }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        logger.info("Received request to delete book with ID: {}", id);
+        bookService.removeBook(id);
+        logger.info("Successfully deleted book with id: {}", id);
+        return ResponseEntity.noContent().build();
     }
 }
