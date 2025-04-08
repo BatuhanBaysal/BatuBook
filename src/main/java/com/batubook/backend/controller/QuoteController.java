9fae8333b0e4bd14218ba3ad96cef1a4a97534d5
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-04-08 08:37:47 +0300

    Update QuoteController.java

--- src/main/java/com/batubook/backend/controller/QuoteController.java
+++ src/main/java/com/batubook/backend/controller/QuoteController.java
@@ -8,71 +8,56 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/quotes")
 @RequiredArgsConstructor
-@CrossOrigin("*")
 public class QuoteController {
 
     private final QuoteServiceInterface quoteService;
     private static final Logger logger = LoggerFactory.getLogger(QuoteController.class);
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<QuoteDTO> createQuote(@Valid @RequestBody QuoteDTO quoteDTO) {
-
-        logger.info("Received request to create a new quote");
-        try {
-            return ResponseEntity.ok(quoteService.createQuote(quoteDTO));
-        } catch (Exception e) {
-            logger.error("Error creating quote", e);
-            return ResponseEntity.internalServerError().build();
-        }
+        logger.info("Received request to create a new quote from userId: {}", quoteDTO.getUserId());
+        QuoteDTO createdQuote = quoteService.registerQuote(quoteDTO);
+        logger.info("Successfully created a new quote with ID: {}", createdQuote.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdQuote);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<QuoteDTO> getQuoteById(@PathVariable Long id) {
-
-        logger.info("Received request to fetch quote with ID: {}", id);
-        try {
-            return ResponseEntity.ok(quoteService.getQuoteById(id));
-        } catch (Exception e) {
-            logger.error("Error fetching quote", e);
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<QuoteDTO> fetchQuoteById(@PathVariable Long id) {
+        logger.info("Received GET request for /api/quotes/{}", id);
+        QuoteDTO quoteDTO = quoteService.getQuoteById(id);
+        logger.info("Returned response for quote with ID: {}", id);
+        return ResponseEntity.ok(quoteDTO);
     }
 
     @GetMapping
-    public ResponseEntity<Page<QuoteDTO>> getAllQuotes(Pageable pageable) {
-
-        logger.info("Received request to fetch all quotes");
-        return ResponseEntity.ok(quoteService.getAllQuotes(pageable));
+    public ResponseEntity<Page<QuoteDTO>> fetchAllQuotes(@PageableDefault(size = 5) Pageable pageable) {
+        logger.info("GET api/quotes called with pagination: page = {}, size = {}", pageable.getPageNumber(), pageable.getPageSize());
+        Page<QuoteDTO> allQuotes = quoteService.getAllQuotes(pageable);
+        logger.info("Successfully fetched {} quotes", allQuotes.getNumberOfElements());
+        return ResponseEntity.ok(allQuotes);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<QuoteDTO> updateQuote(@PathVariable Long id, @Valid @RequestBody QuoteDTO quoteDTO) {
-
         logger.info("Received request to update quote with ID: {}", id);
-        try {
-            return ResponseEntity.ok(quoteService.updateQuote(id, quoteDTO));
-        } catch (Exception e) {
-            logger.error("Error updating quote", e);
-            return ResponseEntity.internalServerError().build();
-        }
+        QuoteDTO updatedQuote = quoteService.modifyQuote(id, quoteDTO);
+        logger.info("Successfully updated quote with ID: {}", id);
+        return ResponseEntity.ok(updatedQuote);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteQuote(@PathVariable Long id) {
-
         logger.info("Received request to delete quote with ID: {}", id);
-        try {
-            quoteService.deleteQuote(id);
-            return ResponseEntity.noContent().build();
-        } catch (Exception e) {
-            logger.error("Error deleting quote", e);
-            return ResponseEntity.internalServerError().build();
-        }
+        quoteService.removeQuote(id);
+        logger.info("Successfully deleted quote with ID: {}", id);
+        return ResponseEntity.noContent().build();
     }
 }
