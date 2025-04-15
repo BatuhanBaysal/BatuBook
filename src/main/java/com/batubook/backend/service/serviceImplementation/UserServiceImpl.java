bdd5fc09074a46e6fc429bc431da6259dcc1fea9
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-04-15 13:08:39 +0300

    Update UserServiceImpl.java

--- src/main/java/com/batubook/backend/service/serviceImplementation/UserServiceImpl.java
+++ src/main/java/com/batubook/backend/service/serviceImplementation/UserServiceImpl.java
@@ -3,12 +3,9 @@ package com.batubook.backend.service.serviceImplementation;
 import com.batubook.backend.dto.UserDTO;
 import com.batubook.backend.entity.UserEntity;
 import com.batubook.backend.entity.UserProfileEntity;
-import com.batubook.backend.entity.enums.Gender;
 import com.batubook.backend.entity.enums.Role;
 import com.batubook.backend.exception.CustomExceptions;
 import com.batubook.backend.mapper.UserMapper;
-import com.batubook.backend.mapper.UserProfileMapper;
-import com.batubook.backend.repository.UserProfileRepository;
 import com.batubook.backend.repository.UserRepository;
 import com.batubook.backend.service.serviceInterface.UserServiceInterface;
 import jakarta.persistence.EntityManager;
@@ -29,8 +26,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 @Service
@@ -42,35 +40,26 @@ public class UserServiceImpl implements UserServiceInterface {
     private final PasswordEncoder passwordEncoder;
     private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
-    private final UserProfileRepository userProfileRepository;
-    private final UserProfileMapper userProfileMapper;
-
     @PersistenceContext
     private EntityManager entityManager;
 
     @Override
     @Transactional
     public UserDTO registerUser(UserDTO userDTO) {
-        logger.info("Starting user creation process for username: {}", userDTO.getUsername());
-
         try {
-            validateUserDTO(userDTO);
+            logger.info("Starting user creation process for username: {}", userDTO.getUsername());
             UserEntity userEntity = userMapper.userDTOToEntity(userDTO);
-            userEntity.setPassword(passwordEncoder.encode(userDTO.getPassword()));
-
-            UserProfileEntity userProfileEntity = mapUserProfile(userDTO);
-            userProfileEntity.setUser(userEntity);
-            userEntity.setUserProfile(userProfileEntity);
-
+            logger.debug("Validating user entity for username: {}", userDTO.getUsername());
+            validateUserEntity(userEntity);
             UserEntity savedUser = userRepository.save(userEntity);
             logger.info("User created successfully: {}", userDTO.getUsername());
             return userMapper.userEntityToDTO(savedUser);
 
         } catch (CustomExceptions.BadRequestException e) {
-            logger.error("Bad Request Error: {}", e.getMessage());
+            logger.error("Bad Request Error while creating user with username {}: {}", userDTO.getUsername(), e.getMessage());
             throw e;
         } catch (Exception e) {
-            logger.error("Error while creating user: {}", e.getMessage());
+            logger.error("Error while creating user with username {}: {}", userDTO.getUsername(), e.getMessage());
             throw new CustomExceptions.InternalServerErrorException("User could not be created: " + e.getMessage());
         }
     }
@@ -119,9 +108,9 @@ public class UserServiceImpl implements UserServiceInterface {
     @Override
     @Transactional(readOnly = true)
     public Page<UserDTO> getUserByCriteria(String searchTerm, Pageable pageable) {
-        logger.info("Searching users with search term: '{}' using pagination: page number = {}, page size = {}",
-                searchTerm, pageable.getPageNumber(), pageable.getPageSize());
         try {
+            logger.info("Searching users with search term: '{}' using pagination: page number = {}, page size = {}",
+                    searchTerm, pageable.getPageNumber(), pageable.getPageSize());
             CriteriaBuilder cb = entityManager.getCriteriaBuilder();
             CriteriaQuery<UserEntity> query = cb.createQuery(UserEntity.class);
             Root<UserEntity> user = query.from(UserEntity.class);
@@ -150,28 +139,27 @@ public class UserServiceImpl implements UserServiceInterface {
     @Transactional
     public UserDTO modifyUser(Long id, UserDTO userDTO) {
         try {
+            logger.info("Attempting to find user with ID: {}", id);
             UserEntity existingUser = userRepository.findById(id)
                     .orElseThrow(() -> {
                         logger.error("User not found with ID: {}", id);
                         return new CustomExceptions.NotFoundException("User not found with ID: " + id);
                     });
 
-            modifyUserDetails(existingUser, userDTO);
-            if (userDTO.getUserProfile() == null) {
-                logger.error("User profile is required for update, user ID: {}", id);
-                throw new CustomExceptions.BadRequestException("User profile is required for update.");
-            }
+            logger.debug("Modifying user details for user ID: {}", id);
+            UserEntity userEntity = userMapper.userDTOToEntity(userDTO);
+            modifyUserDetails(existingUser, userEntity);
 
-            modifyUserProfile(existingUser, userDTO);
             UserEntity updatedUser = userRepository.save(existingUser);
             logger.info("User updated successfully with ID: {}", id);
             return userMapper.userEntityToDTO(updatedUser);
 
         } catch (CustomExceptions.NotFoundException e) {
-            logger.error("User not found: {}", e.getMessage());
+            logger.error("User not found with ID: {}. Error message: {}", id, e.getMessage());
             throw e;
+
         } catch (Exception e) {
-            logger.error("Error while updating user: {}", e.getMessage());
+            logger.error("Error while updating user with ID: {}. Error message: {}", id, e.getMessage());
             throw new CustomExceptions.InternalServerErrorException("User could not be updated: " + e.getMessage());
         }
     }
@@ -189,107 +177,127 @@ public class UserServiceImpl implements UserServiceInterface {
         logger.info("Successfully deleted user with ID: {}", id);
     }
 
-    private void validateUserDTO(UserDTO userDTO) {
-        if (userDTO.getPassword() == null || userDTO.getPassword().isBlank()) {
-            logger.error("Password is empty for user: {}", userDTO.getUsername());
-            throw new CustomExceptions.BadRequestException("Password cannot be empty.");
+    private void validateAndSetField(String field, String value, Consumer<String> setter, String errorMessage, boolean isPassword, boolean isRequired) {
+        if (isRequired && (value == null || value.trim().isEmpty())) {
+            logger.error("{} cannot be null or empty.", field);
+            throw new CustomExceptions.BadRequestException(errorMessage);
         }
-        if (userDTO.getUserProfile() == null) {
-            logger.error("User profile is missing for user: {}", userDTO.getUsername());
-            throw new CustomExceptions.BadRequestException("User profile is required.");
-        }
-    }
 
-    private UserProfileEntity mapUserProfile(UserDTO userDTO) {
-        Gender genderEnum = Gender.fromString(userDTO.getUserProfile().getGender().toString());
-        userDTO.getUserProfile().setGender(genderEnum);
-        return userProfileMapper.userProfileDTOToEntity(userDTO.getUserProfile());
-    }
-
-    private void modifyUserDetails(UserEntity existingUser, UserDTO userDTO) {
-        boolean isUpdated = false;
-        if (userDTO.getUsername() != null && !userDTO.getUsername().isEmpty() && !userDTO.getUsername().equals(existingUser.getUsername())) {
-            existingUser.setUsername(userDTO.getUsername());
-            isUpdated = true;
-        }
-        if (userDTO.getEmail() != null && !userDTO.getEmail().isEmpty() && !userDTO.getEmail().equals(existingUser.getEmail())) {
-            existingUser.setEmail(userDTO.getEmail());
-            isUpdated = true;
-        }
-        if (userDTO.getPassword() != null && !userDTO.getPassword().isEmpty()) {
-            existingUser.setPassword(passwordEncoder.encode(userDTO.getPassword()));
-            logger.info("Password hashed and updated for user ID: {}", existingUser.getId());
-            isUpdated = true;
-        }
-        if (userDTO.getRole() != null && !userDTO.getRole().equals(existingUser.getRole())) {
-            existingUser.setRole(userDTO.getRole());
-            isUpdated = true;
-        }
-        if (isUpdated) {
-            logger.info("User details updated for user ID: {}", existingUser.getId());
-        }
-    }
-
-    private void modifyUserProfile(UserEntity existingUser, UserDTO userDTO) {
-        if (existingUser.getUserProfile() == null) {
-            UserProfileEntity newUserProfile = userProfileMapper.userProfileDTOToEntity(userDTO.getUserProfile());
-            newUserProfile.setUser(existingUser);
-            existingUser.setUserProfile(newUserProfile);
-
-            userProfileRepository.save(newUserProfile);
-            logger.info("Created new user profile for user ID: {}", existingUser.getId());
+        if (value != null && !value.trim().isEmpty()) {
+            if (isPassword) {
+                setter.accept(passwordEncoder.encode(value.trim()));
+                logger.info("{} create/updated (password changed).", field);
+            } else {
+                setter.accept(value);
+                logger.info("{} create/updated: {}", field, value);
+            }
         } else {
-            UserProfileEntity userProfileEntity = existingUser.getUserProfile();
-            boolean profileUpdated = false;
-
-            if (userDTO.getUserProfile() != null) {
-                if (userDTO.getUserProfile().getDateOfBirth() != null) {
-                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-                    userProfileEntity.setDateOfBirth(LocalDate.parse(userDTO.getUserProfile().getDateOfBirth(), formatter));
-                    profileUpdated = true;
-                }
-                if (userDTO.getUserProfile().getGender() != null &&
-                        !userDTO.getUserProfile().getGender().equals(userProfileEntity.getGender())) {
-                    Gender genderEnum = Gender.fromString(userDTO.getUserProfile().getGender().toString());
-                    userProfileEntity.setGender(genderEnum);
-                    profileUpdated = true;
-                }
-                if (userDTO.getUserProfile().getBiography() != null &&
-                        !userDTO.getUserProfile().getBiography().equals(userProfileEntity.getBiography())) {
-                    userProfileEntity.setBiography(userDTO.getUserProfile().getBiography());
-                    profileUpdated = true;
-                }
-                if (userDTO.getUserProfile().getLocation() != null &&
-                        !userDTO.getUserProfile().getLocation().equals(userProfileEntity.getLocation())) {
-                    userProfileEntity.setLocation(userDTO.getUserProfile().getLocation());
-                    profileUpdated = true;
-                }
-                if (userDTO.getUserProfile().getOccupation() != null &&
-                        !userDTO.getUserProfile().getOccupation().equals(userProfileEntity.getOccupation())) {
-                    userProfileEntity.setOccupation(userDTO.getUserProfile().getOccupation());
-                    profileUpdated = true;
-                }
-                if (userDTO.getUserProfile().getEducation() != null &&
-                        !userDTO.getUserProfile().getEducation().equals(userProfileEntity.getEducation())) {
-                    userProfileEntity.setEducation(userDTO.getUserProfile().getEducation());
-                    profileUpdated = true;
-                }
-                if (userDTO.getUserProfile().getInterests() != null &&
-                        !userDTO.getUserProfile().getInterests().equals(userProfileEntity.getInterests())) {
-                    userProfileEntity.setInterests(userDTO.getUserProfile().getInterests());
-                    profileUpdated = true;
-                }
-                if (userDTO.getUserProfile().getProfileImageUrl() != null &&
-                        !userDTO.getUserProfile().getProfileImageUrl().equals(userProfileEntity.getProfileImageUrl())) {
-                    userProfileEntity.setProfileImageUrl(userDTO.getUserProfile().getProfileImageUrl());
-                    profileUpdated = true;
-                }
-            }
-
-            if (profileUpdated) {
-                userProfileRepository.save(userProfileEntity);
-                logger.info("User profile updated for user ID: {}", existingUser.getId());
+            if (isPassword) {
+                logger.error("{} cannot be null or empty.", field);
+                throw new CustomExceptions.BadRequestException(errorMessage);
             }
         }
+    }
+
+    private void validateUserEntity(UserEntity userEntity) {
+        validateAndSetField("Username", userEntity.getUsername(), userEntity::setUsername, "Username cannot be empty.", false, true);
+        validateAndSetField("Email", userEntity.getEmail(), userEntity::setEmail, "Email cannot be empty.", false, true);
+        validateAndSetField("Password", userEntity.getPassword(), userEntity::setPassword, "Password cannot be empty.", true, true);
+
+        if (userEntity.getRole() == null) {
+            logger.error("Role is empty for username: {}", userEntity.getUsername());
+            throw new CustomExceptions.BadRequestException("Role cannot be empty.");
+        } else {
+            logger.info("Role validated for username: {}", userEntity.getUsername());
+        }
+
+        if (userEntity.getUserProfile() == null) {
+            logger.error("User profile is missing for username: {}", userEntity.getUsername());
+            throw new CustomExceptions.BadRequestException("User profile is required.");
+        } else {
+            logger.info("User profile validated for username: {}", userEntity.getUsername());
+
+            UserProfileEntity userProfileEntity = userEntity.getUserProfile();
+            userProfileEntity.setUser(userEntity);
+
+            try {
+                validateAndSetField("Date of Birth", String.valueOf(userProfileEntity.getDateOfBirth()),
+                        value -> userProfileEntity.setDateOfBirth(LocalDate.parse(value)), "Date of Birth cannot be null or empty.", false, true);
+            } catch (DateTimeParseException e) {
+                logger.error("Invalid date format for username: {}", userEntity.getUsername());
+                throw new CustomExceptions.BadRequestException("Invalid date format.");
+            }
+
+            validateAndSetField("Biography", userProfileEntity.getBiography(),
+                    userProfileEntity::setBiography, "Biography cannot be null or empty.", false, true);
+
+            validateAndSetField("Location", userProfileEntity.getLocation(),
+                    userProfileEntity::setLocation, "Location cannot be null or empty.", false, true);
+
+            validateAndSetField("Occupation", userProfileEntity.getOccupation(),
+                    userProfileEntity::setOccupation, "Occupation cannot be null or empty.", false, false);
+
+            validateAndSetField("Education", userProfileEntity.getEducation(),
+                    userProfileEntity::setEducation, "Education cannot be null or empty.", false, false);
+
+            validateAndSetField("Interests", userProfileEntity.getInterests(),
+                    userProfileEntity::setInterests, "Interests cannot be null or empty.", false, false);
+
+            validateAndSetField("Profile Image URL", userProfileEntity.getProfileImageUrl(),
+                    userProfileEntity::setProfileImageUrl, "Profile Image URL cannot be null or empty.", false, false);
+        }
+    }
+
+    private void modifyUserDetails(UserEntity existingUser, UserEntity userEntity) {
+        validateAndSetField("Username", userEntity.getUsername(), existingUser::setUsername, "Username cannot be null or empty.", false,true);
+        validateAndSetField("Email", userEntity.getEmail(), existingUser::setEmail, "Email cannot be null or empty.", false,true);
+        validateAndSetField("Password", userEntity.getPassword(), existingUser::setPassword, "Password cannot be null or empty.", true,true);
+
+        if (userEntity.getRole() != null) {
+            Role roleEnum = Role.fromString(userEntity.getRole().toString());
+            if (roleEnum == null) {
+                logger.error("Invalid role for user Id: {}", userEntity.getId());
+                throw new CustomExceptions.BadRequestException("Invalid role.");
+            }
+            existingUser.setRole(roleEnum);
+            logger.info("Role updated for user ID: {}. New Role: {}", existingUser.getId(), roleEnum);
+        }
+
+        modifyUserProfile(existingUser, userEntity);
+    }
+
+    private void modifyUserProfile(UserEntity existingUser, UserEntity userEntity) {
+        if (existingUser.getUserProfile() == null) {
+            logger.error("User profile is missing for modifyUserProfile, user Id: {}", existingUser.getId());
+            throw new CustomExceptions.BadRequestException("User profile is required for update.");
+        }
+
+        UserProfileEntity userProfileEntity = existingUser.getUserProfile();
+
+        try {
+            validateAndSetField("Date of Birth", String.valueOf(userEntity.getUserProfile().getDateOfBirth()),
+                    value -> userProfileEntity.setDateOfBirth(LocalDate.parse(value)), "Date of Birth cannot be null or empty.", false,true);
+        } catch (DateTimeParseException e) {
+            logger.error("Invalid date format for user ID: {}", existingUser.getId());
+            throw new CustomExceptions.BadRequestException("Invalid date format.");
+        }
+
+        validateAndSetField("Biography", userEntity.getUserProfile().getBiography(),
+                userProfileEntity::setBiography, "Biography cannot be null or empty.", false,true);
+
+        validateAndSetField("Location", userEntity.getUserProfile().getLocation(),
+                userProfileEntity::setLocation, "Location cannot be null or empty.", false,true);
+
+        validateAndSetField("Occupation", userEntity.getUserProfile().getOccupation(),
+                userProfileEntity::setOccupation, "Occupation cannot be null or empty.", false, false);
+
+        validateAndSetField("Education", userEntity.getUserProfile().getEducation(),
+                userProfileEntity::setEducation, "Education cannot be null or empty.", false, false);
+
+        validateAndSetField("Interests", userEntity.getUserProfile().getInterests(),
+                userProfileEntity::setInterests, "Interests cannot be null or empty.", false, false);
+
+        validateAndSetField("Profile Image URL", userEntity.getUserProfile().getProfileImageUrl(),
+                userProfileEntity::setProfileImageUrl, "Profile Image URL cannot be null or empty.", false, false);
     }
 }
